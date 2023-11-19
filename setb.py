542d452116ee386ca13b4508073bd0907ca055a8
Nidhi sahu <149890086+NidhiSahu2@users.noreply.github.com>
2023-11-19 10:09:40 +0530

    Add files via upload

---
+++ setb.py
@@ -0,0 +1,10 @@
+# two set addition#
+hero_name={'ironman',' captain','american','hulk'}
+non_hero_name={'tony','steve','bruce',}
+hero_name | non_hero_name
+new_avengers={'ant man','wrap','spiderman'}
+
+heros={'tony','burce','dr.strande'}
+villains ={'thanos','loki','doramaamu'}
+heros.isdisjoint(villains)
+
